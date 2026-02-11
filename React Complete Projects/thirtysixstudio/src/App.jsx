a971f65003e1e6af0b2cc90fae04b75a4c20519c
Johan <iam.masoom92@gmail.com>
2026-02-11 23:23:42 +0530

    dev

--- React Complete Projects/thirtysixstudio/src/App.jsx
+++ React Complete Projects/thirtysixstudio/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Screen from './Screen'
+import data from './data'
 
 const App = () => {
   return (
     <div className='bg-black min-h-screen w-full text-white flex flex-wrap'>
-      <Screen startIndex={0} />
-      <Screen startIndex={150}/>
-      <Screen startIndex={300}/>
-      <Screen startIndex={450}/>
-      <Screen startIndex={600}/>
-      <Screen startIndex={750}/>
-      <Screen startIndex={900}/>
+      {data.map((item, index)=>(
+        <div key={index}>
+          {item.map((screendetas, index)=>{
+            <Screen details={screendetas} />
+          })}
+        </div>
+      ))}
     </div>
   )
 }
